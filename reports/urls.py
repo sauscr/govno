9b4d97ecfbb12d11e74fd3219edcdadca34195b2
p0mpilium <navermore322@gmail.com>
2024-09-13 14:59:52 +0500

    redact 2 tablo for humans

--- reports/urls.py
+++ reports/urls.py
@@ -1,9 +1,12 @@
+# reports/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.view_table_one, name='indicator_one'),  # one/
-    path(' /edit/<int:id>/', views.edit_tableone, name='edit_indicator_one'),  # Редактирование
-    path(' /delete/<int:id>/', views.delete_tableone, name='delete_indicator_one'),  # Удаление
+    path('edit/<int:id>/', views.edit_tableone, name='edit_indicator_one'),  # Редактирование
+    path('delete/<int:id>/', views.delete_tableone, name='delete_indicator_one'),  # Удаление
     path('two/', views.view_table_two, name='indicator_two'),
+    path('two/edit/<int:id>/', views.edit_tabletwo, name='edit_indicator_two'),  # Редактирование для table two
+    path('two/delete/<int:id>/', views.delete_tabletwo, name='delete_indicator_two'),  # Удаление для table two
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,43 +1,23 @@
 # reports/views.py
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import JsonResponse
 from .models import TableOne, TableTwo
 from .forms import TableOneForm, TableTwoForm
 
 def view_table_one(request):
-    """
-    View-функция для отображения и добавления данных первой таблицы.
-    """
-    
-    
     if request.method == 'POST':
         form = TableOneForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('indicator_one')
-
     else:
         form = TableOneForm()
     
     indicators = TableOne.objects.all()
 
-    return render(request,'reports/tableone.html',
-                  {'form': form,
-                   'indicators': indicators})
-
-    indicators = TableOne.objects.all()
-
-    context = {
-        'form': TableOneForm(),
-        'indicators': indicators
-    }
-
-    return render(request, 'reports/tableone.html', context)
+    return render(request, 'reports/tableone.html', {'form': form, 'indicators': indicators})
 
 def edit_tableone(request, id):
-    """
-    Функция для редактирования данных индикатора.
-    """
     indicator = get_object_or_404(TableOne, id=id)
     if request.method == 'POST':
         form = TableOneForm(request.POST, instance=indicator)
@@ -47,22 +27,17 @@ def edit_tableone(request, id):
     else:
         form = TableOneForm(instance=indicator)
     
+    if request.is_ajax():
+        return render(request, 'reports/partials/edit_tableone_form.html', {'form': form})
+    
     return render(request, 'reports/tableone.html', {'form': form})
 
 def delete_tableone(request, id):
-    """
-    Функция для удаления индикатора.
-    """
     indicator = get_object_or_404(TableOne, id=id)
     indicator.delete()
     return redirect('indicator_one')
 
-
-
 def view_table_two(request):
-
-    context = 'Крутое название сайта'
-
     if request.method == 'POST':
         form = TableTwoForm(request.POST)
         if form.is_valid():
@@ -70,10 +45,27 @@ def view_table_two(request):
             return redirect('indicator_two')
     else:
         form = TableTwoForm()
-
+    
     indicators = TableTwo.objects.all()
 
-    return render(request, 'reports/tabletwo.html',
-                  {'form': form,
-                   'indicators': indicators,
-                   })
+    return render(request, 'reports/tabletwo.html', {'form': form, 'indicators': indicators})
+
+def edit_tabletwo(request, id):
+    indicator = get_object_or_404(TableTwo, id=id)
+    if request.method == 'POST':
+        form = TableTwoForm(request.POST, instance=indicator)
+        if form.is_valid():
+            form.save()
+            return redirect('indicator_two')
+    else:
+        form = TableTwoForm(instance=indicator)
+    
+    # if request.is_ajax():
+    #     return render(request, 'reports/edit_tabletwo.html', {'form': form})
+    
+    return render(request, 'reports/tabletwo.html', {'form': form})
+
+def delete_tabletwo(request, id):
+    indicator = get_object_or_404(TableTwo, id=id)
+    indicator.delete()
+    return redirect('indicator_two')
