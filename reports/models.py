d78fd0a6621c7e6e1e83ab0db75ad977a58702e4
p0mpilium <navermore322@gmail.com>
2024-09-17 19:15:45 +0500

    Add sqlite3 to gitignore

--- reports/models.py
+++ reports/models.py
@@ -1,6 +1,6 @@
 # from django.conf import settings
 from django.db import models
-from django.db.models import JSONField
+# from django.db.models import JSONField
 
 from .services.logic import CalculationServices
 
